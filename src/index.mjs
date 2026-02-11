d9593417c05ff44a6b15bed210e2adb0f0834513
Petr Tripolsky <tripolskypetr@gmail.com>
2026-02-12 00:28:19 +0500

    patch

--- src/index.mjs
+++ src/index.mjs
@@ -4,11 +4,14 @@ import {
   addFrameSchema,
   addStrategySchema,
   Cache,
+  Constant,
+  commitPartialProfit,
+  addActionSchema,
 } from "backtest-kit";
 
-import { extract, run, toSignalDto } from "@backtest-kit/pinets";
+import { extract, run, toSignalDto, File } from "@backtest-kit/pinets";
 
-import { singleshot, str } from "functools-kit";
+import { singleshot, str, randomString } from "functools-kit";
 import { generateText, Output } from "ai";
 import { createGroq } from "@ai-sdk/groq";
 import { z } from "zod";
@@ -128,6 +131,7 @@ addExchangeSchema({
   },
 });
 
+/*
 addRiskSchema({
   riskName: "signal_confidence_risk",
   validations: [
@@ -155,6 +159,34 @@ addRiskSchema({
       },
     },
   ],
+});
+*/
+
+addActionSchema({
+  actionName: "partial_profit_action",
+  handler: class {
+    async partialProfitAvailable({ symbol, level }) {
+      if (level === Constant.TP_LEVEL3) {
+        await commitPartialProfit(symbol, 33);
+      }
+      if (level === Constant.TP_LEVEL2) {
+        await commitPartialProfit(symbol, 33);
+      }
+      if (level === Constant.TP_LEVEL1) {
+        await commitPartialProfit(symbol, 34);
+      }
+    }
+  },
+});
+
+addActionSchema({
+  actionName: "breakeven_action",
+  handler: class {
+    async breakevenAvailable({ symbol, currentPrice }) {
+      // Lower trailing-stop by 3 points (negative value brings stop-loss closer to entry)
+      await commitTrailingStop(symbol, -3, currentPrice);
+    }
+  }
 });
 
 addStrategySchema({
@@ -185,7 +217,13 @@ addStrategySchema({
 
     return await toSignalDto(signalId, data_lower, null);
   },
-  riskList: ["signal_confidence_risk"],
+  riskList: [
+    // "signal_confidence_risk"
+  ],
+  actions: [
+    "partial_profit_action",
+    "breakeven_action",
+  ]
 });
 
 addFrameSchema({
