58d6f42d8f348e1dfd52cc3e8736e160593c7d85
Petr Tripolsky <tripolskypetr@gmail.com>
2026-04-06 18:41:04 +0500

    wip

--- scripts/run_signal.ts
+++ scripts/run_signal.ts
@@ -1,3 +1,3 @@
 import { signal } from "../logic";
 
-console.log(await signal("BTCUSDT", new Date("2026-04-05T11:00:00.000Z")));
+console.log(await signal("BTCUSDT", new Date("2026-04-06T11:00:00.000Z")));
